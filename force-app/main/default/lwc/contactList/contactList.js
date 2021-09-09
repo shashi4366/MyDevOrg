135aace894ebb96308d00111e9a746b63adb2f6e
ShashiSirim <shashi@telcowin.com>
2021-09-09 15:58:33 +0530

    comment

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,14 +3,14 @@ import { reduceErrors } from 'c/ldsUtils';
 import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME from '@salesforce/schema/Contact.LastName';
 import EMAIL from '@salesforce/schema/Contact.Email'; 
-import getContacts from '@salesforce/apex/ContactController.getContacts';
+//import getContacts from '@salesforce/apex/ContactController.getContacts';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text' },
     { label: 'Annual Revenue', fieldName: LAST_NAME.fieldApiName, type: 'text' },
     { label: 'EMAIL', fieldName: EMAIL.fieldApiName, type: 'email' }];
 export default class ContactList extends LightningElement {
     
-    columns = COLUMNS;
+  /*  columns = COLUMNS;
     @wire(getContacts)
     contact;
 
@@ -18,6 +18,6 @@ export default class ContactList extends LightningElement {
         return (this.contact.error) ?
             reduceErrors(this.contact.error) : [];
     }
-        
+        */
 
 }
